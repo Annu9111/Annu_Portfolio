2bfe87acfb06f663014d1bb909b672ecc24846bf
Annu9111 <aksoni911190@gmail.com>
2026-03-21 20:17:02 +0530

    js

---
+++ script.js
@@ -0,0 +1,51 @@
+// Mobile Menu Toggle
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+const sections = document.querySelectorAll(".section");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+// Typing Effect
+const text = ["Web Developer", "Python Developer", "DSA Learner"];
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = text[i];
+
+  if (isDeleting) {
+    j--;
+  } else {
+    j++;
+  }
+
+  document.querySelector(".typing").innerText = current.substring(0, j);
+
+  if (!isDeleting && j === current.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
